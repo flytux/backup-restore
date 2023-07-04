4b61058590b59c9c7e71960e308fbc15ade32314
Jaehoon <hooney.jung@gmail.com>
2023-07-05 08:36:05 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,31 +24,7 @@ echo 'source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
 echo 'source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >>~/.zshrc
 
-# Install krew
-curl -LO https://dl.k8s.io/release/v1.25.9/bin/linux/amd64/kubectl
-
-chmod +x kubectl && mv kubectl /usr/local/bin
-
-(
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-)
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-
-kubectl krew install ctx
-kubectl krew install ns 
-
-# zshrc
-
-plugins=(
-  kube-ps1
-)
-PROMPT='$(kube_ps1)'$PROMPT
+cat << EOF >> ~/.zshrc
 
 source <(kubectl completion zsh)
 
@@ -59,5 +35,5 @@ alias kc='kubectl config use-context'
 alias kcg='kubectl config get-contexts'
 alias di='docker images --format "table {{.Repository}}:{{.Tag}}\t{{.ID}}\t{{.Size}}\t{{.CreatedSince}}"'
 alias kge="kubectl get events  --sort-by='.metadata.creationTimestamp'  -o 'go-template={{range .items}}{{.involvedObject.name}}{{\"\t\"}}{{.involvedObject.kind}}{{\"\t\"}}{{.message}}{{\"\t\"}}{{.reason}}{{\"\t\"}}{{.type}}{{\"\t\"}}{{.firstTimestamp}}{{\"\n\"}}{{end}}'"
-
+EOF
 
